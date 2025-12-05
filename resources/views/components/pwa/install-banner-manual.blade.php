eddb345d1771ef7eecf892e39c6dbd99b72622cf
Jonas Weigert <weigert.jonas@gmail.com>
2025-12-05 12:16:29 -0600

    wip

--- resources/views/components/pwa/install-banner-manual.blade.php
+++ resources/views/components/pwa/install-banner-manual.blade.php
@@ -1,7 +1,7 @@
 {{-- resources/views/components/pwa/install-banner-manual.blade.php --}}
 <div x-cloak x-data="manualInstallBanner" x-show="show" class="bg-yellow-600 text-white px-4 py-3 flex items-center justify-between z-50 shadow-lg">
     <span>
-        <strong>Install this app manually</strong> for a better experience!
+        <div class="mb-1"><strong>Install this app manually</strong> for a better experience!</div>
         <template x-if="isIos()">
             <span class="ml-2">Tap <span class="font-bold">Share</span> <svg class="inline w-4 h-4" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M4 12v.01M12 4v16m8-8v.01M16 8l-4-4-4 4"/></svg> then <span class="font-bold">Add to Home Screen</span>.</span>
         </template>
