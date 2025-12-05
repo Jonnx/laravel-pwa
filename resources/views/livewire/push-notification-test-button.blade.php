804428ea186a33b4866ada4bf4cfc510456d6092
Jonas Weigert <weigert.jonas@gmail.com>
2025-12-05 13:18:09 -0600

    wip

--- resources/views/livewire/push-notification-test-button.blade.php
+++ resources/views/livewire/push-notification-test-button.blade.php
@@ -1,7 +1,7 @@
 <div>
     <button
         wire:click="sendTestNotification"
-        class="bg-green-600 text-white font-semibold px-4 py-2 rounded shadow hover:bg-green-700 transition cursor-pointer"
+        class="bg-white text-black font-semibold px-4 py-2 rounded shadow hover:bg-neutral-100 transition cursor-pointer"
     >
         Send Test Push Notification
     </button>
