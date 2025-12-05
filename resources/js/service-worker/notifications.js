8fec737b2aeba4e746301b7767d73f736a576f6f
Jonas Weigert <weigert.jonas@gmail.com>
2025-12-05 13:21:34 -0600

    wip

--- resources/js/service-worker/notifications.js
+++ resources/js/service-worker/notifications.js
@@ -20,3 +20,30 @@ self.addEventListener('push', (event) => {
 
     self.registration.showNotification(payload.title, payload);
 });
+
+self.addEventListener('notificationclick', function(event) {
+    self.navigator.clearAppBadge();
+
+    // DEBUG: log event and notification
+    // console.log(event);
+    // console.log(event.notification);
+
+    // dismiss notification when clicked
+    event.notification.close();
+
+
+    // check notification data
+    if(event.notification.data) {
+        if(event.notification.data.url_open) {
+            event.waitUntil(
+                (async () => {
+                    const clients = await self.clients.matchAll();                    
+                    if(clients.length > 0) {
+                        clients[0].navigate(event.notification.data.url_open);
+                        clients[0].focus();
+                    }
+                })()
+            );
+        }
+    }
+});
