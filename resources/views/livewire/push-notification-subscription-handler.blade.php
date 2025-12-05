a6ccc8fba98618efbb4cfc79e1c9e0902ffa18ba
Jonas Weigert <weigert.jonas@gmail.com>
2025-12-05 08:58:38 -0600

    wip

--- resources/views/livewire/push-notification-subscription-handler.blade.php
+++ resources/views/livewire/push-notification-subscription-handler.blade.php
@@ -1,5 +1,17 @@
 {{-- resources/views/livewire/push-notification-subscription-handler.blade.php --}}
-<div>
+<div x-data="PushNotificationSubscriptionHandler()" x-init="init()">
     <!-- Livewire logic for push notification subscription goes here -->
     <p>Push Notification Subscription Handler Component Loaded.</p>
 </div>
+
+<script>
+    function PushNotificationSubscriptionHandler() {
+        return {
+            init() {
+                console.log('Push Notification Subscription Handler Initialized');
+                // Add your push notification subscription logic here
+            }
+        }
+    }
+</script>
+{{-- end of livewire/push-notification-subscription-handler --}}
