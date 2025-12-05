eb97f0acb8515d5a6e012f0aa52d1350f0401406
Jonas Weigert <weigert.jonas@gmail.com>
2025-12-05 09:10:11 -0600

    wip

--- resources/views/livewire/push-notification-subscription-handler.blade.php
+++ resources/views/livewire/push-notification-subscription-handler.blade.php
@@ -1,25 +1,28 @@
 {{-- resources/views/livewire/push-notification-subscription-handler.blade.php --}}
-<div x-data="PushNotificationSubscriptionHandler()">
+<div x-data="pushNotificationSubscriptionHandler">
     <!-- Livewire logic for push notification subscription goes here -->
     <p>Push Notification Subscription Handler Component Loaded.</p>
 </div>
 
 <script>
-    function PushNotificationSubscriptionHandler() {
-        return {
+    document.addEventListener('alpine:init', () => {
+        Alpine.data('pushNotificationSubscriptionHandler', () => ({
+            debug: {{ $debug ? 'true' : 'false' }},
+             log(message) {
+                if (this.debug) {
+                    console.log(message);
+                }
+            },
             init() {
                 this.log('Push Notification Subscription Handler Initialized');
-                this.checkSubscription(false)
+                this.checkSubscription(false);
             },
-            log(message) {
-                if({{ $debug ? 'true' : 'false' })
-                    console.log(message);
-            } 
+           
             checkSubscription(userInitiated = false) {
                 // Logic to ask for push notification permission
-                this.log('Push Notification Subscription Permission Requested', {userInitiated});
+                this.log('Push Notification Subscription Permission Requested', { userInitiated });
             }
-        }
-    }
+        }));
+    });
 </script>
 {{-- end of livewire/push-notification-subscription-handler --}}
