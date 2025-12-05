52cbd78f4c98392bb219e9fac4315f1ce143bc63
Jonas Weigert <weigert.jonas@gmail.com>
2025-12-05 13:16:21 -0600

    wip

--- resources/views/livewire/push-notification-test-button.blade.php
+++ resources/views/livewire/push-notification-test-button.blade.php
@@ -1,6 +1,6 @@
 <div>
     <button
-        @click="sendTestNotification"
+        @wire:click="sendTestNotification"
         class="bg-green-600 text-white font-semibold px-4 py-2 rounded shadow hover:bg-green-700 transition cursor-pointer"
     >
         Send Test Push Notification
